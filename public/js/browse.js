f16e5695e9e1de14d5d1a6e5bdfd995b89db6dbd
Mateus Santos <33528161+uaumateus@users.noreply.github.com>
2018-05-27 14:25:16 -0300

    Página de navegar implementada (faltando ainda o modo aleatório)

---
+++ public/js/browse.js
@@ -0,0 +1,43 @@
+var words = [
+    {text: "Lorem", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Ipsum", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Dolor", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Dolor", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Lorem", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Ipsum", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Dolor", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Dolor", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Lorem", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Ipsum", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Dolor", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Dolor", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Lorem", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Ipsum", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Dolor", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Dolor", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Lorem", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Ipsum", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Dolor", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Dolor", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Lorem", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Ipsum", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Dolor", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Dolor", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Lorem", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Ipsum", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Dolor", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Dolor", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Lorem", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Ipsum", weight: (Math.random() * 5) + 1, link: '#'},
+    {text: "Dolor", weight: (Math.random() * 5) + 1, link: '#'},
+  
+    /* ... */
+  ];
+  
+  $('.grid-tags').jQCloud(words, {
+    delay: 10,
+    fontSize: {
+        from: 0.15,
+        to: 0.04
+      }
+  });
