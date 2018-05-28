1686fd60cf0d8556c8b55a65ca5a7dba0efc145f
Mateus Santos <33528161+uaumateus@users.noreply.github.com>
2018-05-28 15:50:56 -0300

    Pagina de editar perfil implementada

---
+++ public/js/editProfile.js
@@ -0,0 +1,13 @@
+function checkPortfolio() {
+    // Get the checkbox
+    var checkBox = document.getElementById("slideOne");
+    // Get the output text
+    var btnDisplay = document.getElementById("editPortfolio");
+  
+    // If the checkbox is checked, display the output text
+    if (checkBox.checked == true){
+        btnDisplay.style.display = "block";
+    } else {
+        btnDisplay.style.display = "none";
+    }
+  }
