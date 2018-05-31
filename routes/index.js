2af599265b0f86f92ce4174b348fd47d0c89bd5f
paulocardosob <paulocardosob@gmail.com>
2018-05-31 20:21:58 -0300

    Uso inicial de EJS

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  let emCartazItem = {
+    "name": "PQPQP",
+    "description": "HUE",
+    "exibitionLocation": "Caralho's house"
+  };
+
+  let emCartaz = [];
+
+  for (let i = 0; i < 5; i++){
+    emCartaz.push(emCartazItem);
+  }
+  
+  res.render('index', {emCartazList: emCartaz});
 });
+
+router.get("pages/recommended.html", function(req,res, next) {
+  res.render('recommended');
+})
 
 module.exports = (app) => app.use("/", router) 
