59030cab45b8afc6441394fb166cf0e7269a5ec2
paulocardosob <paulocardosob@gmail.com>
2018-06-15 15:11:22 -0300

    Pagina de browser

--- database/index.js
+++ database/index.js
@@ -3,6 +3,6 @@ const mongoose = require("mongoose")
 // mongoose.connect("mongodb://paulocardosob:12345678987654321@ds235840.mlab.com:35840/cineflixsmd")
 // mongoose.connect("mongodb://localhost:27017/cineFlix");
 mongoose.connect("mongodb://nloadingsmd:a1234567890b@ds041851.mlab.com:41851/ucine");
-mongoose.Promise = global.Promise
+// mongoose.Promise = global.Promise
 
 module.exports = mongoose
