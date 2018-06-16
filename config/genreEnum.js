16c471e2bdd144f8aa1789413e2499c69e96a8e6
paulocardosob <paulocardosob@gmail.com>
2018-06-16 17:28:08 -0300

    Botao de random linkado com o servidor

--- config/genreEnum.js
+++ config/genreEnum.js
@@ -2,13 +2,13 @@ const GenreEnum = Object.freeze({
     "Ação": "5b1dac4f0841e3035a9621fc",
     "Animação": "5b1dac590841e3035a9621fd",
     "Aventura": "5b1dac720841e3035a9621fe",
-    "Curtas": "5b1dac7b0841e3035a9621ff",
+    "Curta": "5b1dac7b0841e3035a9621ff",
     "Documentários": "5b1dac8b0841e3035a962200",
     "Drama": "5b1dac900841e3035a962201",
     "Fantasia": "5b1dac960841e3035a962202",
     "FicçãoCientífica": "5b1dacacdc3ff104e039c485",
     "Suspense": "5b1dacb1dc3ff104e039c486",
     "Terror": "5b1dacb5dc3ff104e039c487",
-})
+}) 
 
 module.exports = GenreEnum
