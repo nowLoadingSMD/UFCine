1b3e1e134ead2c23e9b2f0f7f9d8146cd948e588
paulocardosob <paulocardosob@gmail.com>
2018-06-18 18:47:59 -0300

    More changes on model

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken")
 
 const authConfig = require("../config/auth")
 
-const User = require("../models/user") 
+const User = require("../models/User") 
 
 const router = express.Router()
 
