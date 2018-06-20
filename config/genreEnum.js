06d2374a736c5329142394bd509632a57598914c
paulocardosob <paulocardosob@gmail.com>
2018-06-19 22:30:16 -0300

    Adiçao de videos

--- config/genreEnum.js
+++ config/genreEnum.js
@@ -1,31 +1,31 @@
 //MLAB
 
-const GenreEnum = Object.freeze({
-    "Ação": "5b1dac4f0841e3035a9621fc",
-    "Animação": "5b1dac590841e3035a9621fd",
-    "Aventura": "5b1dac720841e3035a9621fe",
-    "Curta": "5b1dac7b0841e3035a9621ff",
-    "Documentários": "5b1dac8b0841e3035a962200",
-    "Drama": "5b1dac900841e3035a962201",
-    "Fantasia": "5b1dac960841e3035a962202",
-    "FicçãoCientífica": "5b1dacacdc3ff104e039c485",
-    "Suspense": "5b1dacb1dc3ff104e039c486",
-    "Terror": "5b1dacb5dc3ff104e039c487",
-})
+// const GenreEnum = Object.freeze({
+//     "Ação": "5b1dac4f0841e3035a9621fc",
+//     "Animação": "5b1dac590841e3035a9621fd",
+//     "Aventura": "5b1dac720841e3035a9621fe",
+//     "Curta": "5b1dac7b0841e3035a9621ff",
+//     "Documentários": "5b1dac8b0841e3035a962200",
+//     "Drama": "5b1dac900841e3035a962201",
+//     "Fantasia": "5b1dac960841e3035a962202",
+//     "FicçãoCientífica": "5b1dacacdc3ff104e039c485",
+//     "Suspense": "5b1dacb1dc3ff104e039c486",
+//     "Terror": "5b1dacb5dc3ff104e039c487",
+// })
 
 
 //Local
-// const GenreEnum = Object.freeze({
-//     "Ação": "5b27c169ae76dc29794b9320",
-//     "Animação": "5b27c173ae76dc29794b9321",
-//     "Aventura": "5b27c17dae76dc29794b9322",
-//     "Curta": "5b27c183ae76dc29794b9323",
-//     "Documentários": "5b27c189ae76dc29794b9324",
-//     "Drama": "5b27c18fae76dc29794b9325",
-//     "Fantasia": "5b27c194ae76dc29794b9326",
-//     "FicçãoCientífica": "5b27c19aae76dc29794b9327",
-//     "Suspense": "5b27c1a1ae76dc29794b9328",
-//     "Terror": "5b27c1abae76dc29794b9329",
-// }) 
+const GenreEnum = Object.freeze({
+    "Ação": "5b27c169ae76dc29794b9320",
+    "Animação": "5b27c173ae76dc29794b9321",
+    "Aventura": "5b27c17dae76dc29794b9322",
+    "Curta": "5b27c183ae76dc29794b9323",
+    "Documentários": "5b27c189ae76dc29794b9324",
+    "Drama": "5b27c18fae76dc29794b9325",
+    "Fantasia": "5b27c194ae76dc29794b9326",
+    "FicçãoCientífica": "5b27c19aae76dc29794b9327",
+    "Suspense": "5b27c1a1ae76dc29794b9328",
+    "Terror": "5b27c1abae76dc29794b9329",
+}) 
 
 module.exports = GenreEnum
